7f812bce49e0990fca86376528124e6e1e1c4888
MrWhiteShapi <aigunovshapi@yandex.ru>
2024-09-16 11:29:24 +0300

    Создал словарь слов и UI шаблоны

--- src/main/java/backend/academy/gallows/GameSession.java
+++ src/main/java/backend/academy/gallows/GameSession.java
@@ -1,12 +1,54 @@
 package backend.academy.gallows;
 
+import backend.academy.gallows.model.Categories;
+import backend.academy.gallows.model.Levels;
+import backend.academy.gallows.model.Word;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GameSession {
     private final String playerName;
 
+    private Categories category;
+    private Levels level;
+    private Word word;
+
+    private int hitPoints;
+
     public GameSession(final String name) {
         this.playerName = name;
+    }
+
+    /**
+     * Основной метод игровой сессии
+     */
+    void session(){
+
+    }
+
+    /**
+     * Метод определяет отгадываемое слово
+     * @param level - какой уровень сложности хочет игрок
+     * @param category - какую тему для слов выбрал игрок
+     * @return
+     */
+    Word createWord(final Levels level, final Categories category){
+        return null;
+    }
+
+    /**
+     * Метод проверяет, есть ли в слове введенная буква и сколько раз он повторяется
+     * @param letter
+     * @return
+     */
+    int[] calculateLetters(final Character letter){
+        return null;
+    }
+
+    /**
+     * Метод-цикл задающий и определяющий меню, и устанваливает настройки игры
+     */
+    void menuCircle(){
+
     }
 }
